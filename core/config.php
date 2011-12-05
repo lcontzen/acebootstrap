74ea5518c6235282b68cf712b72a5cf44d0b461a
Laurent <lcontzen@gmail.com>
2011-12-05 23:46:12 +0100

    Added mysql fields to core/config.php and started var names change from camelCase to _ed notation

--- core/config.php
+++ core/config.php
@@ -1,27 +1,36 @@
 <?php
 Class Config {
-  private $siteName;
-  private $siteUrl;
-  private $siteTheme;
+  private $mysql_srv_addr;
+  private $mysql_srv_port;
+  private $mysql_srv_user;
+  private $mysql_db_name;
+  private $site_name;
+  private $site_url;
+  private $site_theme;
 
   public function __construct () {
 	$ini = parse_ini_file(getcwd() . "/config.ini", true);
+	$mysql = $ini['mysql'];
+	$this->mysql_srv_addr = $mysql['srv_addr'];
+	$this->mysql_srv_port = $mysql['srv_port'];
+	$this->mysql_server_user = $mysql['srv_user'];
+	$this->mysql_db_name = $mysql['db_name'];
 	$site = $ini['site'];
-	$this->siteName = $site['name'];
-	$this->siteUrl = $site['url'];
-	$this->siteTheme = $site['theme'];
+	$this->site_name = $site['name'];
+	$this->site_url = $site['url'];
+	$this->site_theme = $site['theme'];
   }
 
-  public function getSiteName() {
-	return $this->siteName;
+  public function get_site_name() {
+	return $this->site_name;
   }
 
-  public function getSiteUrl() {
-	return $this->siteUrl;
+  public function get_site_url() {
+	return $this->site_url;
   }
   
-  public function getSiteTheme() {
-	return $this->siteTheme;
+  public function get_site_theme() {
+	return $this->site_theme;
   }
 
 }
