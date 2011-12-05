b09e7b95b73b4f5e093ae4de13849da8ed461701
Laurent <lcontzen@gmail.com>
2011-12-05 23:13:01 +0100

    Real addition of data/db.sql

---
+++ data/db.sql
@@ -0,0 +1,23 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+DROP DATABASE `ace`;
+CREATE DATABASE `ace` DEFAULT CHARACTER SET utf8;
+USE `ace`;
+
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE IF NOT EXISTS `Users` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Email` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `Login` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `Password` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `News`;
+CREATE TABLE IF NOT EXISTS `News` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `Abstract` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
