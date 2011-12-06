9cc4ca1103daf7479ca23d46319c22e90c158315
Laurent <lcontzen@gmail.com>
2011-12-07 00:08:44 +0100

    Require once core/auth.php and create new Auth instance in index.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 require_once("core/config.php");
+require_once("core/auth.php");
 require_once("core/data.php");
 require_once("core/user.php");
 
 try {
+  $auth = new Auth();
   $config = new Config();
   $action = isset ($_GET['action']) ? $_GET['action'] : "home";
   include("pages/page.inc");
